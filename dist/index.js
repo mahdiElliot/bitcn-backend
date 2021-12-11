5e99cb063dfdfdbd9cd9118e2e71683c556dc43c
mahdij <mahdimn2011@yahoo.com>
2021-12-11 10:46:38 +0330

    changes

--- dist/index.js
+++ dist/index.js
@@ -22,7 +22,6 @@ const request_1 = __importDefault(require("request"));
 const index_1 = __importDefault(require("./db/index"));
 const fs_1 = __importDefault(require("fs"));
 const csv_parser_1 = __importDefault(require("csv-parser"));
-const poloniex_1 = __importDefault(require("./models/poloniex"));
 const itbit_1 = __importDefault(require("./models/itbit"));
 const port = process.env.PORT || 8081;
 const app = (0, express_1.default)();
@@ -69,7 +68,7 @@ const start = () => __awaiter(void 0, void 0, void 0, function* () {
         // addBits('docs/Bitfinex_BTCUSD_1h.csv', Bitfinex)
         // addBits('docs/Bitstamp_BTCUSD_1h.csv', Bitstamp)
         // addBits('docs/Bittrex_BTCUSD_1h.csv', Bittrex)
-        addBits('docs/Poloniex_BTCUSDT_1h.csv', poloniex_1.default);
+        // addBits('docs/Poloniex_BTCUSDT_1h.csv', Poloniex)
         addBits('docs/Itbit_BTCUSD_1h.csv', itbit_1.default);
         // app.listen(port, () => {
         // })
