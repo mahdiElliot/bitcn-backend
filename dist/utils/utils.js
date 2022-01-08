231e32aef14fe888818f77fdf664aac99a95441a
mahdiElliot <50835200+mahdiElliot@users.noreply.github.com>
2022-01-08 13:36:01 +0330

    Delete dist directory
    dist deleted

--- dist/utils/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    oil_token: () => '66881aa0e3aa76138862828949d0eb1c',
-    oil_api_url_data: (date = 'past_day') => `https://api.oilpriceapi.com/v1/prices/${date}`
-};
-//# sourceMappingURL=utils.js.map
